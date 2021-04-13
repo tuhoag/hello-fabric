87a2ebf778ec69605498b18c1bbc77f4b0a7e7ea
Tu Hoang <hatu87@gmail.com>
2021-04-13 15:41:45 +0200

    finish adding channel

--- main.sh
+++ main.sh
@@ -92,8 +92,28 @@ function startNetwork() {
 function stopNetwork() {
   infoln "Stopping the network"
 
+  # docker-compose -f $COMPOSE_FILE_BASE -f $COMPOSE_FILE_COUCH -f $COMPOSE_FILE_CA down --volumes --remove-orphans
+  # docker-compose -f $COMPOSE_FILE_COUCH_ORG3 -f $COMPOSE_FILE_ORG3 down --volumes --remove-orphans
+  # # Don't remove the generated artifacts -- note, the ledgers are always removed
+  # if [ "$MODE" != "restart" ]; then
+  #   # Bring down the network, deleting the volumes
+  #   #Cleanup the chaincode containers
+  #   clearContainers
+  #   #Cleanup images
+  #   removeUnwantedImages
+  #   # remove orderer block and other channel configuration transactions and certs
+  #   docker run --rm -v $(pwd):/data busybox sh -c 'cd /data && rm -rf system-genesis-block/*.block organizations/peerOrganizations organizations/ordererOrganizations'
+  #   ## remove fabric ca artifacts
+  #   docker run --rm -v $(pwd):/data busybox sh -c 'cd /data && rm -rf organizations/fabric-ca/org1/msp organizations/fabric-ca/org1/tls-cert.pem organizations/fabric-ca/org1/ca-cert.pem organizations/fabric-ca/org1/IssuerPublicKey organizations/fabric-ca/org1/IssuerRevocationPublicKey organizations/fabric-ca/org1/fabric-ca-server.db'
+  #   docker run --rm -v $(pwd):/data busybox sh -c 'cd /data && rm -rf organizations/fabric-ca/org2/msp organizations/fabric-ca/org2/tls-cert.pem organizations/fabric-ca/org2/ca-cert.pem organizations/fabric-ca/org2/IssuerPublicKey organizations/fabric-ca/org2/IssuerRevocationPublicKey organizations/fabric-ca/org2/fabric-ca-server.db'
+  #   docker run --rm -v $(pwd):/data busybox sh -c 'cd /data && rm -rf organizations/fabric-ca/ordererOrg/msp organizations/fabric-ca/ordererOrg/tls-cert.pem organizations/fabric-ca/ordererOrg/ca-cert.pem organizations/fabric-ca/ordererOrg/IssuerPublicKey organizations/fabric-ca/ordererOrg/IssuerRevocationPublicKey organizations/fabric-ca/ordererOrg/fabric-ca-server.db'
+  #   docker run --rm -v $(pwd):/data busybox sh -c 'cd /data && rm -rf addOrg3/fabric-ca/org3/msp addOrg3/fabric-ca/org3/tls-cert.pem addOrg3/fabric-ca/org3/ca-cert.pem addOrg3/fabric-ca/org3/IssuerPublicKey addOrg3/fabric-ca/org3/IssuerRevocationPublicKey addOrg3/fabric-ca/org3/fabric-ca-server.db'
+  #   # remove channel and script artifacts
+  #   docker run --rm -v $(pwd):/data busybox sh -c 'cd /data && rm -rf channel-artifacts log.txt *.tar.gz'
+  # fi
+
   COMPOSE_FILE="-f ${COMPOSE_FILE_BASE}"
-  IMAGE_TAG=$IMAGETAG docker-compose ${COMPOSE_FILE} down 2>&1
+  IMAGE_TAG=$IMAGETAG docker-compose ${COMPOSE_FILE} down --volumes --remove-orphans 2>&1
 }
 
 function clearOutputs() {
@@ -101,7 +121,7 @@ function clearOutputs() {
 }
 
 FABRIC_LOGGING_SPEC=DEBUG
-CHANNEL_NAME="mychannel"
+CHANNEL_NAME="mychannel1"
 DELAY="3"
 MAX_RETRY="2"
 VERBOSE="false"
@@ -161,13 +181,34 @@ function createChannel() {
 	verifyResult $res "Channel creation failed"
 }
 
+
+function joinChannel() {
+  FABRIC_CFG_PATH=$OUTPUTS
+  ORG=$1
+  setGlobals $ORG
+	local rc=1
+	local COUNTER=1
+	## Sometimes Join takes time, hence retry
+	while [ $rc -ne 0 -a $COUNTER -lt $MAX_RETRY ] ; do
+    sleep $DELAY
+    set -x
+    peer channel join -b $BLOCKFILE >&log.txt
+    res=$?
+    { set +x; } 2>/dev/null
+		let rc=$res
+		COUNTER=$(expr $COUNTER + 1)
+	done
+	cat log.txt
+	verifyResult $res "After $MAX_RETRY attempts, peer0.org${ORG} has failed to join channel '$CHANNEL_NAME' "
+}
+
 function verifyResult() {
   if [ $1 -ne 0 ]; then
     fatalln "$2"
   fi
 }
 
-setGlobals() {
+function setGlobals() {
   local USING_ORG=""
   if [ -z "$OVERRIDE_ORG" ]; then
     USING_ORG=$1
@@ -177,12 +218,13 @@ setGlobals() {
   infoln "Using organization ${USING_ORG}"
   if [ $USING_ORG -eq 1 ]; then
     export CORE_PEER_LOCALMSPID="Org1MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/${ORGANIZATION_OUTPUTS}/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+    # $PEER0_ORG1_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/${ORGANIZATION_OUTPUTS}/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
     export CORE_PEER_ADDRESS=localhost:7051
   elif [ $USING_ORG -eq 2 ]; then
     export CORE_PEER_LOCALMSPID="Org2MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/${ORGANIZATION_OUTPUTS}/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
     export CORE_PEER_MSPCONFIGPATH=${PWD}/${ORGANIZATION_OUTPUTS}/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
     export CORE_PEER_ADDRESS=localhost:9051
 
@@ -234,8 +276,13 @@ elif [ "$MODE" == "up" ]; then
 elif [ "$MODE" == "createChannel" ]; then
   createChannelTx
   createChannel
+  # setGlobals 1
+  joinChannel 1
+  # setGlobals 2
+  joinChannel 2
 elif [ "$MODE" == "down" ]; then
   stopNetwork
+  clearOutputs
 elif [ "$MODE" == "clean" ]; then
   clearOutputs
 fi
